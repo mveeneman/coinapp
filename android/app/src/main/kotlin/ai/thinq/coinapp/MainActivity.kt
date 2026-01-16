2f400447b14dbf197f7a8e40135c9a5025bd2f80
Mourad Veeneman <6137667+mveeneman@users.noreply.github.com>
2026-01-16 16:52:31 +0100

    first commit

---
+++ android/app/src/main/kotlin/ai/thinq/coinapp/MainActivity.kt
@@ -0,0 +1,7 @@
+﻿// Ultralytics ðŸš€ AGPL-3.0 License - https://ultralytics.com/license
+
+package ai.thinq.coinapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
